8496b1cb5e9635151760aae96ffd3df1b704fe86
manis-h <85171340+manis-h@users.noreply.github.com>
2025-09-01 11:46:04 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -14,7 +14,7 @@ export default function Register({ setToken }) {
     }
 
     try {
-      await axios.post('http://localhost:3000/api/auth/register', { email, password });
+      await axios.post('https://dsasheet-production.up.railway.app/api/auth/register', { email, password });
       const res = await axios.post('http://localhost:3000/api/auth/login', { email, password });
       setToken(res.data.token);
     } catch (err) {
